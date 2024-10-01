35490e9b77c66f5f2d545d773c62c16949e2d5ce
SolisPDev <solispantonio@gmail.com>
2024-09-30 21:37:36 -0600

    Actualizacion menu

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get('/welcome', function () {
+        return view('welcome');
+    })->name('welcome');
+});
